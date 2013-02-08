d0f18bd630c992561028e613d00045cbad5cadb9
robinharms <robin@betahaus.net>
2013-02-08 14:40:42 +0100

    cleanup of login forms. Don't render forms if there's nothing to do

--- voteit/cloudsignon/components.py
+++ voteit/cloudsignon/components.py
@@ -6,24 +6,31 @@ from voteit.cloudsignon import VoteITCSO as _
 from voteit.cloudsignon.fanstaticlib import voteit_cso 
 
 
-@view_action('login_forms', 'cloudsignon')
-def cloudsignon(context, request, va, **kw):
+def login_provider(context, request, va, **kw):
     voteit_cso.need()
-    response = {'api': kw['api'],
-                'login_url': login_url,
-                'came_from': request.GET.get('came_from', ''),
-                'providers': request.registry.settings['login_providers'],}
-    
-    return render('templates/login.pt', response, request = request)
-
-@view_action('connect_forms', 'cloudsignconnect')
-def cloudsignconnect(context, request, va, **kw):
-    voteit_cso.need()
+    providers = request.registry.settings.get('login_providers', ())
+    if va.name not in providers:
+        return u""
     api = kw['api']
-    user = api.user_profile
-    response = {'api': api,
-                'user': user,
-                'login_url': login_url,
-                'providers': request.registry.settings['login_providers'],}
-    
-    return render('templates/connect.pt', response, request = request)
+    response = dict(
+        api = api,
+        came_from = request.GET.get('came_from', ''),
+        title = va.title,
+    )   
+    return render(va.kwargs['template'], response, request = request)
+
+@view_action('login_forms', 'facebook', title = _(u"Facebook"), template="templates/facebook.pt")
+@view_action('login_forms', 'twitter', title = _(u"Twitter"), template="templates/twitter.pt")
+def login_va(context, request, va, **kw):
+    api = kw['api']
+    if not api.userid:
+        return login_provider(context, request, va, **kw)
+    return u""
+
+@view_action('connect_forms', 'facebook', title = _(u"Connect to Facebook"), template="templates/facebook.pt")
+@view_action('connect_forms', 'twitter', title = _(u"Connect to Twitter"), template="templates/twitter.pt")
+def connect_va(context, request, va, **kw):
+    api = kw['api']
+    if api.userid and va.name not in api.user_profile.auth_domains:
+        return login_provider(context, request, va, **kw)
+    return u""  
