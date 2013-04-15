2ab844ed68bd22fe6e349395990b93d0a65943d3
robinharms <robin@betahaus.net>
2013-04-15 10:17:13 +0200

    not used

--- voteit/cloudsignon/fanstaticlib.py
+++ voteit/cloudsignon/fanstaticlib.py
@@ -2,9 +2,7 @@
 from fanstatic import Library
 from fanstatic import Resource
 
-from voteit.core.fanstaticlib import voteit_main_css
+#from voteit.core.fanstaticlib import voteit_main_css
 
 
 voteit_cso_lib = Library('voteit_cso', 'static')
-
-voteit_cso = Resource(voteit_cso_lib, 'voteit_cso.css', depends=(voteit_main_css,))
